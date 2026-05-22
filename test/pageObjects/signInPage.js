9c0bf41e9311e92658e9159f38ec05d236bce96b
Dan Rusu <dangingko@yahoo.com>
2026-05-22 15:31:05 +0300

    fixes

--- test/pageObjects/signInPage.js
+++ test/pageObjects/signInPage.js
@@ -1,24 +1,34 @@
 import { $, driver } from '@wdio/globals';
 
 async function signIn(username, password) {
-  await driver.pause(10000);
-
   const signInButton = await $('//*[@text="Sign In"]');
+  await signInButton.waitForDisplayed({
+    timeout: 20_000,
+  });
   await signInButton.click();
 
   const webViewContext = await waitForWebView(20_000);
-  await browser.switchContext(webViewContext);
+  console.log('@@@ Found Webview context:', webViewContext);
+  await driver.switchContext(webViewContext);
 
-  const usernameInput = await $('//*[@resource-id="username"]');
-  await username.waitForDisplayed({
-    timeout: 20_000,
+  const handles = await driver.getWindowHandles();
+  console.log(`@@@ Window handles: ${handles}`);
+  await driver.switchToWindow(handles[0]);
+  const url = await driver.getUrl();
+  console.log(`@@@ Webview current URL: ${url}`);
+
+  const usernameInput = await $('#username');
+  await usernameInput.waitForDisplayed({
+    timeout: 10_000,
+    interval: 1000,
+    timeoutMsg: 'Username input not displayed after 10s',
   });
   await usernameInput.setValue(username);
 
-  const passwordInput = await $('//*[@resource-id="password"]');
+  const passwordInput = await $('#password');
   await passwordInput.setValue(password);
 
-  const logInButton = await $('//*[@text="LOG IN"]');
+  const logInButton = await $('//*[contains(@text, "LOG IN")]');
   await logInButton.click();
 }
 
@@ -28,9 +38,11 @@ async function waitForWebView(timeout = 10_000) {
   await browser.waitUntil(
     async () => {
       const contexts = await browser.getContexts();
-      console.log('Available contexts:', contexts);
+      console.log('@@@ Available contexts:', contexts);
 
-      webViewContext = contexts.find(ctx => ctx.includes('webView'));
+      webViewContext = contexts.find(ctx =>
+        ctx.toLowerCase().includes('webview'),
+      );
 
       return !!webViewContext;
     },
