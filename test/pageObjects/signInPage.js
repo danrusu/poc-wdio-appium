4c6f0631f7599d8eb4f024e577feba4a83b85bb9
Dan Rusu <dangingko@yahoo.com>
2026-05-22 13:41:48 +0300

    fix

--- test/pageObjects/signInPage.js
+++ test/pageObjects/signInPage.js
@@ -6,22 +6,8 @@ async function signIn(username, password) {
   const signInButton = await $('//*[@text="Sign In"]');
   await signInButton.click();
 
-  let webview;
-  await driver.waitUntil(
-    () => {
-      const contexts = await driver.getContexts();
-      console.log(`@@@ contexts: ${contexts}`);
-      webview = contexts.find(context => context.includes('WEBVIEW'));
-      return !!webview;
-    },
-    {
-      timeout: 20_000,
-      interval: 500,
-      timeoutMsg: 'Expected to find a WEBVIEW context after 20s',
-    },
-  );
-
-  await driver.switchContext(webview);
+  const webViewContext = await waitForWebView(20_000);
+  await browser.switchContext(webViewContext);
 
   const usernameInput = await $('//*[@resource-id="username"]');
   await username.waitForDisplayed({
@@ -34,6 +20,28 @@ async function signIn(username, password) {
 
   const logInButton = await $('//*[@text="LOG IN"]');
   await logInButton.click();
+}
+
+async function waitForWebView(timeout = 10_000) {
+  let webViewContext;
+
+  await browser.waitUntil(
+    async () => {
+      const contexts = await browser.getContexts();
+      console.log('Available contexts:', contexts);
+
+      webViewContext = contexts.find(ctx => ctx.includes('webView'));
+
+      return !!webViewContext;
+    },
+    {
+      timeout,
+      interval: 500,
+      timeoutMsg: `webView context not found in ${timeout} ms`,
+    },
+  );
+
+  return webViewContext;
 }
 
 export { signIn };
